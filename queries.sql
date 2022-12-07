170a0af221837a5a24f68b4acda0c5953b899c80
Kelvin Ben <kevoben14@gmail.com>
2022-12-07 23:58:40 +0300

    Alter the table animals to add a column

--- queries.sql
+++ queries.sql
@@ -6,3 +6,37 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = True;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <= 17.3;
+
+ALTER TABLE animals ADD COLUMN species VARCHAR;
+BEGIN;
+  UPDATE animals SET species = 'unspecified';
+BEGIN;
+  UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+  UPDATE animals SET species = 'pokemon' WHERE species IS NULL OR species = '';
+BEGIN;
+  DELETE FROM animals;
+BEGIN;
+  DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+  SAVEPOINT SP1;
+  UPDATE animals SET weight_kg = weight_kg * -1;
+  ROLLBACK TO SP1;
+  UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT 
+  weight_kg,
+  AVG(weight_kg)
+  FROM animals GROUP BY weight_kg;
+  SELECT 
+    neutered,
+    MAX(escape_attempts)
+    FROM animals
+    GROUP BY neutered;
+  SELECT 
+    weight_kg,
+    MIN(weight_kg),MAX(weight_kg) FROM animals 
+    GROUP BY weight_kg;
+  SELECT date_of_birth,
+    AVG(escape_attempts),
+    date_of_birth BETWEEN '1990-12-01' AND '2000-12-01'
+    FROM animals GROUP BY date_of_birth;
