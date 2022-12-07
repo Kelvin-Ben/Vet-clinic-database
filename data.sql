10914beef90d00212f92d8bd018878683d21ed50
Kelvin Ben <kevoben14@gmail.com>
2022-12-07 23:57:01 +0300

    Alter the table animals to add a column

--- data.sql
+++ data.sql
@@ -2,4 +2,11 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Agumon', '2020-02-03', 0, True, 10.23),
         ('Gabumon', '2018-11-15', 2, True, 8),
         ('Pikachu', '2021-01-07', 1, False, 15.04),
-        ('Davimon', '2017-05-12', 5, True, 11);
+        ('Davimon', '2017-05-12', 5, True, 11),
+        ('Charmander', '2020-02-08', 0, False, -11),
+        ('Plantmon', '2021-11-15', 2, True, -5.7),
+        ('Squirtle', '1993-04-02', 3, False, -12.13),
+        ('Angemon', '2005-06-12', 1, True, -45),
+        ('Boarmon', '2005-06-07', 7, True, 20.4),
+        ('Blossom', '1998-10-13', 3, True, 17),
+        ('Ditto', '2022-05-14', 4, True, 22);
